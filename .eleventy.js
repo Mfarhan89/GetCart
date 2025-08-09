a5963e2688fdb20a8f4e4c4d2b75c2bc30bd60be
Mfarhan89 <farhan782@gmail.com>
2025-08-10 00:19:19 +0800

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,9 @@
-
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({ "src/_redirects": "_redirects" });
+  // Copy assets & CMS if needed
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/admin");
-  eleventyConfig.addPassthroughCopy("src/_redirects");
+
   return {
     dir: {
       input: "src",
