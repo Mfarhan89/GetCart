7295372e3e3754a815e73551ec573c4d6e2b3350
Mfarhan89 <farhan782@gmail.com>
2025-08-09 23:56:30 +0800

    Add files via upload

---
+++ .eleventy.js
@@ -0,0 +1,10 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/admin");
+  return {
+    dir: {
+      input: "src",
+      output: "_site"
+    }
+  };
+};
